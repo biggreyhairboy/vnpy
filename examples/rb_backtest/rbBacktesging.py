535ac987467dda0c06ad04226d5d75103925e3ef
patrick <biggreyhairboy@gmail.com>
2018-10-03 14:43:34 +0800

    doublma strategy working for minute bar of rb1901

--- examples/rb_backtest/rbBacktesging.py
+++ examples/rb_backtest/rbBacktesging.py
@@ -6,37 +6,35 @@
 
 from __future__ import division
 
-
 from vnpy.trader.app.ctaStrategy.ctaBacktesting import BacktestingEngine, MINUTE_DB_NAME
 
-
 if __name__ == '__main__':
-    from vnpy.trader.app.ctaStrategy.strategy.strategyKingKeltner import KkStrategy
-    
+    from vnpy.trader.app.ctaStrategy.strategy.strategyDoubleMa import DoubleMaStrategy
+
     # 创建回测引擎
     engine = BacktestingEngine()
-    
+
     # 设置引擎的回测模式为K线
     engine.setBacktestingMode(engine.BAR_MODE)
 
     # 设置回测用的数据起始日期
-    engine.setStartDate('20120101')
-    
+    engine.setStartDate('20180117')
+
     # 设置产品相关参数
-    engine.setSlippage(0.2)     # 股指1跳
-    engine.setRate(0.3/10000)   # 万0.3
-    engine.setSize(300)         # 股指合约大小 
-    engine.setPriceTick(0.2)    # 股指最小价格变动
-    
+    engine.setSlippage(1)  # 1跳
+    engine.setRate(5 / 10000)  # 万0.3
+    engine.setSize(10)  # 合约大小
+    engine.setPriceTick(1)  # 最小价格变动
+
     # 设置使用的历史数据库
-    engine.setDatabase(MINUTE_DB_NAME, 'rb1901')
-    
+    engine.setDatabase(MINUTE_DB_NAME, 'RB0000')
+
     # 在引擎中创建策略对象
     d = {}
-    engine.initStrategy(KkStrategy, d)
-    
+    engine.initStrategy(DoubleMaStrategy, d)
+
     # 开始跑回测
     engine.runBacktesting()
-    
+
     # 显示回测结果
     engine.showBacktestingResult()
